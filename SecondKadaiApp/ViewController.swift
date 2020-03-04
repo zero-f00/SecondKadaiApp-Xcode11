e93d58774c68ea5c7c237dec922e9665a9342862
yuuto.masamura <yuto.jl8nml@gmail.com>
2020-03-04 19:43:27 +0900

    2つ目の課題提出

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // UITextFieldで取得した値を格納する変数
+    @IBOutlet weak var TextField: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のSecondViewControllerを取得する
+        let secondViewController:SecondViewController = segue.destination as! SecondViewController
+        
+        // 遷移先のSecondViewControllerで宣言している、変数nameに値を渡す
+        secondViewController.name = TextField.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきたときに呼ばれる
+    }
 
 
 }
